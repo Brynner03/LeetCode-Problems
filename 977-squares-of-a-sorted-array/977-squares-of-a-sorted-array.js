e13ef5023d5f09579136f1c841c7976378d93c5a
Brynner Ventura <100214729+Brynner03@users.noreply.github.com>
2022-06-09 14:54:52 -0400

    Time: 133 ms (60.46%), Space: 48.3 MB (91.72%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -3,13 +3,33 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
- const sortedSquares = new Array(nums.length).fill(0);
+  // Create a new Array = array length
+  const sortedSquare = new Array(nums.length)
+  // Create a variable to track smaller index = 0
+  let smallerValueIdx = 0
+  // Create a variable to track larger index = last index
+  let largerValueIdx = nums.length - 1
 
-  for(let i = 0 ; i < nums.length; i++) {
-    const value = nums[i]
-    sortedSquares[i] = value * value 
-  }
-  sortedSquares.sort((a,b) => a - b)
-  return sortedSquares
+  // Loop through the array starting at the end, decrementing.
+  for(let idx = nums.length - 1 ; idx >= 0; idx--){
+    // Create a variable = array(smaller index)
+  const smallerValue = nums[smallerValueIdx]
+    // Create a variable = array(larger index)
+  const largerValue = nums[largerValueIdx]
   
+    // Check if the absolute value of the smaller index is greater than larger
+  if (Math.abs(smallerValue) > Math.abs(largerValue)) {
+      // if it is set the index of the new array equal to smaller * itself
+      // Increment smaller index
+    sortedSquare[idx] = smallerValue * smallerValue
+    smallerValueIdx++
+  } else {
+      // Set sortedSquare index = largerValue * itself
+      // Decrement largerValueIdx
+    sortedSquare[idx] = largerValue * largerValue
+    largerValueIdx--
+    }
+  }
+  // Return the new Array
+  return sortedSquare
 }
