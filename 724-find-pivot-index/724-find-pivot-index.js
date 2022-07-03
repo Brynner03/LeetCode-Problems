f8e9e188faf1f7186ce274380402a84f5269fd71
Brynner Ventura <100214729+Brynner03@users.noreply.github.com>
2022-07-03 08:46:44 -0400

    Time: 193 ms (27.42%), Space: 46.4 MB (20.24%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    
+    let total = 0
+    for (const n in nums) {
+        total += n
+    }
+    
+    let leftSum = 0
+    let rightSum = nums.reduce((a,b) => a + b)
+    for(let i = 0; i < nums.length; i++) {
+        rightSum -= nums[i]
+        if (leftSum == rightSum) {
+            return i
+        }
+        leftSum += nums[i]
+    }
+    return -1
+};
