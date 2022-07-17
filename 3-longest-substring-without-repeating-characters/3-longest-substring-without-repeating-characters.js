c62e023f778369e9127bd3c85a2666a24a11f1f9
Brynner Ventura <100214729+Brynner03@users.noreply.github.com>
2022-07-16 21:36:15 -0400

    Time: 124 ms (70.66%), Space: 48 MB (56.10%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,24 +3,22 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    // Time Complexity: 0(N^2)
-    // Space Complexity: 0(N)
-    let counter = 0
-    
     if(s.length <= 1) return s.length;
     
-    for (let left = 0; left < s.length; left++) {
-        let seenChars = {}, currentLength = 0;
-        for (let right = left; right < s.length; right++) {
-            const currentChar = s[right]
-            if (!seenChars[currentChar]) {
-                currentLength++;
-                seenChars[currentChar] = true
-                counter = Math.max(counter, currentLength)
-            } else {
-                break;
-            }
+    const seenChars = {}
+    let counter = 0, left = 0
+
+    
+    for (let right = 0; right < s.length; right++) {
+        const currentChar = s[right]
+        const prevSeenChar = seenChars[currentChar]
+        
+        if(prevSeenChar >= left) {
+            left = prevSeenChar + 1
         }
+        
+        seenChars[currentChar] = right
+        counter = Math.max(counter, right - left + 1)
     }
     return counter
 };
