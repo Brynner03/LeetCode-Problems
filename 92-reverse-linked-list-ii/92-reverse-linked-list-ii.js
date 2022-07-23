e592ca789a22602f399ee6bcc8265a4418089e53
Brynner Ventura <100214729+Brynner03@users.noreply.github.com>
2022-07-22 21:52:52 -0400

    Time: 89 ms (46.26%), Space: 41.7 MB (93.70%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+    
+    let currentNode = head
+    let currentPosition = 1
+    let start = head
+    
+    while (currentPosition < left) {
+        start = currentNode
+        currentNode = currentNode.next
+        currentPosition++
+    }
+    let newList = null
+    let tail = currentNode
+    while (currentPosition >=left && currentPosition <= right) {
+        const next = currentNode.next
+        currentNode.next = newList
+        newList = currentNode
+        currentNode = next
+        currentPosition++
+    }
+    
+    start.next = newList
+    tail.next = currentNode
+    
+    if (left > 1) {
+        return head
+    } else {
+        return newList
+    }
+    
+};
