82fd4832a58de8433c6d9e6dae5d3c3d7dccdb4d
Brynner Ventura <100214729+Brynner03@users.noreply.github.com>
2022-07-25 20:47:13 -0400

    Time: 87 ms (44.79%), Space: 42.2 MB (32.28%) - LeetHub

---
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.js
@@ -0,0 +1,68 @@
+
+var MyQueue = function() {
+    this.in = []
+    this.out = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    // Time: 0(N)
+    // Space: 0(N)
+    this.in.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    // Space: 0(N)
+    // Time: 0(N)
+    if(this.out.length === 0) {
+        while(this.in.length) {
+            this.out.push(this.in.pop())
+        }
+    }
+    return this.out.pop()
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    
+    // Space: 0(N)
+    // Time: 0(N)
+    
+    if (this.out.length === 0) {
+        while(this.in.length) {
+            this.out.push(this.in.pop())
+        }
+    }
+    return this.out[this.out.length - 1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    
+    // Time: 0(1)
+    // Space: 0(1)
+    
+    if(this.in.length === 0 && this.out.length === 0) {
+        return true
+    }
+    return false
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
