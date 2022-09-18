4436117c94bb3e08ae88a716abbef427385a0153
Brynner Ventura <100214729+Brynner03@users.noreply.github.com>
2022-09-18 04:15:30 -0400

    Time: 126 ms (35.31%), Space: 44.5 MB (71.60%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -6,11 +6,12 @@ var isPalindrome = function(s) {
     
     // Replace Everything that is not alphanumeric to an empty space
     s = s.replace(/[^A-Za-z0-9]/g,'').toLowerCase()
+    
     let left = 0
     let right = s.length - 1
     
     while(left < right) {
-        if(s[left] !== s[right]) {
+        if (s[left] !== s[right]) {
             return false
         }
         left++
