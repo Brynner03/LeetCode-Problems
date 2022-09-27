b8da5921777fbbcbba2bd9d2c83f9cd48ff7c610
Brynner Ventura <100214729+Brynner03@users.noreply.github.com>
2022-09-26 23:27:08 -0400

    Time: 48 ms (99.88%), Space: 42 MB (93.77%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -4,26 +4,13 @@
  */
 var runningSum = function(nums) {
     
-    // Do we assume that the array is sorted?
-    // Does it matter if it is sorted or not?
-    
-    // Do I return a list of the new array?
-    // Create a new array
-    // Loop through the array 
-    // Add the previous index to the current index
-    // 2 Pointers
-    
-    let newNums = []
-    let currentSum = 0
+    let newArr = []
+    let runner = 0
     
     for (let i = 0; i < nums.length; i++) {
-        // Keeping track of the value
-        let currentValue = nums[i]
-        // Adding value to our sum on every loop
-        currentSum += currentValue
-        // Adding the current sum to our array
-        newNums.push(currentSum)
+        runner += nums[i]
+        nums[i] += runner
+        newArr.push(runner)
     }
-    return newNums
-    
+    return newArr
 };
